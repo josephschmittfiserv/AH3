ddd2b3edc980f2f3f85ac13332f2bf73abc09a45
F93AB7X <joseph.schmitt1@fiserv.com>
2023-10-16 15:46:23 -0400

    Recursive downwards approach

--- AH3.java
+++ AH3.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class AH3 {
+    public static void fizzbuzz(int rand) {
+        if (rand == 0) return;
+
+        if (rand % 5 == 0 && rand % 3 == 0) System.out.println("FizzBuzz");
+        else if (rand % 5 == 0 && rand % 3 != 0) System.out.println("Buzz");
+        else if (rand % 3 == 0 && rand % 5 == 0) System.out.println("FizzBuzz");
+        else System.out.println(rand);
+
+        fizzbuzz(rand - 1);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter in a number to see the fizzbuzz: ");
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 != 0) System.out.println("Fizz");
-            else if (i % 5 == 0 && i % 3 != 0) System.out.println("Buzz");
-            else if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
-            else System.out.println(i);
-        }
+        fizzbuzz(n);
     }
 }
